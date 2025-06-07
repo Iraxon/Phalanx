5892c5fbfc30a1fe632d5986d26fa62f6d1bc20e
Iraxon <156947097+Iraxon@users.noreply.github.com>
2025-06-07 16:12:29 -0400

    Fixed a bug with blockItems

--- src/main/java/net/iraxon/phalanx/registration/BlockElement.java
+++ src/main/java/net/iraxon/phalanx/registration/BlockElement.java
@@ -2,14 +2,12 @@ package net.iraxon.phalanx.registration;
 
 import java.util.function.Function;
 
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 
 public record BlockElement(RegisterManager registerManager, String name,
         Function<BlockBehaviour.Properties, ? extends Block> constructor,
-        BlockBehaviour.Properties properties, boolean blockItem)
+        BlockBehaviour.Properties properties/*, boolean blockItem*/)
         implements ConstructedElement<BlockBehaviour.Properties, Block> {
 
     @Override
@@ -25,22 +23,22 @@ public record BlockElement(RegisterManager registerManager, String name,
     @Override
     public RegisterManager register() {
         var r = ConstructedElement.super.register();
-        if (blockItem) {
-            registerManager.newElementFromSupplier(name, BlockItem.class, () -> new BlockItem(
-                    registerManager.get(Block.class, name).get(), new Item.Properties()));
-        }
+        // if (blockItem) {
+        //     registerManager.newElementFromSupplier(name, BlockItem.class, () -> new BlockItem(
+        //             registerManager.get(Block.class, name).get(), new Item.Properties()));
+        // }
         return r;
     }
 
     public BlockElement constructor(Function<BlockBehaviour.Properties, ? extends Block> blockConstructor) {
-        return new BlockElement(registerManager, name, blockConstructor, properties, blockItem);
+        return new BlockElement(registerManager, name, blockConstructor, properties/*, blockItem*/);
     }
 
     public BlockElement properties(BlockBehaviour.Properties properties) {
-        return new BlockElement(registerManager, name, constructor, properties, blockItem);
+        return new BlockElement(registerManager, name, constructor, properties/*, blockItem*/);
     }
 
     public BlockElement withoutBlockItem() {
-        return new BlockElement(registerManager, name, constructor, properties, false);
+        return new BlockElement(registerManager, name, constructor, properties/*, false*/);
     }
 }
