6de818f9cd0a037065b6d50768b86759a658e56b
Iraxon <156947097+Iraxon@users.noreply.github.com>
2025-10-28 20:23:53 -0400

    Added Warhorn sound

--- src/main/java/net/iraxon/phalanx/PhalanxMod.java
+++ src/main/java/net/iraxon/phalanx/PhalanxMod.java
@@ -6,10 +6,16 @@ import com.mojang.logging.LogUtils;
 
 import net.iraxon.phalanx.claim.ClaimBlock;
 import net.iraxon.phalanx.registration.RegisterManager;
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -17,6 +23,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
 
 @Mod(PhalanxMod.MOD_ID)
 public class PhalanxMod {
@@ -27,14 +34,14 @@ public class PhalanxMod {
     public PhalanxMod(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
         modEventBus.addListener(this::commonSetup);
-        modEventBus.addListener(this::onServerStarting);
 
         regm = new RegisterManager(MOD_ID, modEventBus);
 
         regm.newBlock("claim_block").constructor(ClaimBlock::new).register();
         regm.newBlockItem("claim_block").tab(CreativeModeTabs.FUNCTIONAL_BLOCKS).register();
 
-        regm.newItem("warhorn").properties(new Item.Properties().fireResistant()).tab(CreativeModeTabs.COMBAT).register();
+        regm.newItem("warhorn").properties(new Item.Properties().fireResistant()).tab(CreativeModeTabs.COMBAT)
+                .register();
 
         regm.build();
 
@@ -44,6 +51,22 @@ public class PhalanxMod {
     private void commonSetup(final FMLCommonSetupEvent event) {
     }
 
+    @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
+    }
+
+    @SubscribeEvent
+    public void warhornFunctionality(LivingEntityUseItemEvent.Start event) {
+        if (event != null
+                && !event.isCanceled()
+                && event.getEntity() != null
+                && event.getItem().is(regm.getInstance(ForgeRegistries.ITEMS, "warhorn"))) {
+            event.getEntity().level().playLocalSound(
+                    event.getEntity().blockPosition(),
+                    SoundEvents.RAID_HORN.get(),
+                    SoundSource.MASTER,
+                    16f, 0.5f,
+                    false);
+        }
     }
 }
